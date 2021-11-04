07f7bab96f5e721dd4407563a083f60b105a0577
Scott Hillier <scotthhillier@gmail.com>
2021-11-03 17:06:02 -0700

    Word Search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -7,11 +7,11 @@ describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['S', 'E', 'I', 'N', 'C', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'R', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'A', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'N', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'K', 'E', 'Y', 'B'],
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
@@ -22,13 +22,13 @@ describe("#wordSearch()", function() {
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'A', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['R', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['N', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
+    let result = [];
+    let counter = 0;
+    for (let i = 0; i < letters.length; i++) {
+        for (let j= 0; j < letters[i].length; j++) {
+            if (letters[i][j] === word[0]) {
+                for (let k = i; k < letters.length - i; k++) {
+                    if (letters[k][j] === word[counter]) {
+                        result.push(letters[k][j]);
+                        counter += 1;
+                        if (result.length === word.length) {
+                            return true;
+                        }
+                    } else {
+                        result = [];
+                        counter = 0;
+                        k = letters.length;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+};
 
 module.exports = wordSearch
